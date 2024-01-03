1a2e3a60b2f37eb95cd84efcea076958a469d013
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2024-01-03 18:48:54 +0500

    Show total items near cart link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { ButtonIcon, LinkIcon } from '../miniComponents/ClickableSquare';
 import SiteTitle from '../miniComponents/SiteTitle';
+import { useNumCartProducts } from '../hooks/useCartData';
 
 const HeaderElem = styled.header`
   border-bottom: 1px solid black;
@@ -15,15 +16,36 @@ const HeaderDiv = styled.div`
 
 const LinkRight = styled(LinkIcon)`
   margin-left: auto;
+
+  position: relative;
+`;
+
+const NumProducts = styled.span`
+  position: absolute;
+  left: 1.5rem;
+  bottom: 1.75rem;
+  background: #2d3342;
+  border-radius: 50%;
+  width: 1.5rem;
+  height: 1.5rem;
+  color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 function Header({ menuButton }) {
+  const { numCartProducts } = useNumCartProducts();
+
   return (
     <HeaderElem>
       <HeaderDiv>
         <ButtonIcon onClick={menuButton} symbolText="menu" />
         <SiteTitle />
-        <LinkRight symbolText="shopping_cart" to="/cart" />
+        <LinkRight symbolText="shopping_cart" to="/cart">
+          <NumProducts>{numCartProducts}</NumProducts>
+        </LinkRight>
       </HeaderDiv>
     </HeaderElem>
   );
